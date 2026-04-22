df79a401b6ed14bfd8d15435851738e445bf5439
Николай <kolyan7328@mail.ru>
2026-04-22 16:46:53 +0300

    init commit

---
+++ src/test/java/org/example/lms_platform/LmsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lms_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
